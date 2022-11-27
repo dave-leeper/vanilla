73a11487a5ca613830cf1b3d2a2adb4c858cb883
David Leeper <davidleeper@Davids-Mac-mini.lan>
2022-11-26 18:55:58 -0700

    Added ability to recursively include files.

--- src/js/include.js
+++ src/js/include.js
@@ -1,16 +1,25 @@
-document.addEventListener(
-    "DOMContentLoaded",
-    function(){
-        let includes = document.getElementsByTagName('include');
-        for(var i=0; i<includes.length; i++){
-            let include = includes[i];
-            load_file(includes[i].attributes.src.value, function(text){
-                include.insertAdjacentHTML('afterend', text);
-                include.remove();
-            });
-        }
-        function load_file(filename, callback) {
-            fetch(filename).then(response => response.text()).then(text => callback(text));
-        }
+const loadIncludes = () => {
+    let includes = document.getElementsByTagName('include');
+    /*
+    for(var i=0; i<includes.length; i++){
+        let include = includes[i];
+        loadFile(include.attributes.src.value, function(text){
+            include.insertAdjacentHTML('afterend', text);
+            include.remove();
+        });
     }
-);
+    */
+   if (0 === includes.length) {return}
+    let include = includes[0];
+    loadFile(include.attributes.src.value, function(text){
+        include.insertAdjacentHTML('afterend', text);
+        include.remove();
+        loadIncludes();
+    });
+}
+
+const loadFile = (filename, callback) => {
+    fetch(filename).then(response => response.text()).then(text => callback(text));
+}
+
+document.addEventListener("DOMContentLoaded", loadIncludes);
