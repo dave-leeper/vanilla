6006bf58423c3169635a785dc2f7ba14c6d6340a
David Leeper <davidleeper@Davids-Mac-mini.lan>
2022-12-03 09:52:17 -0700

    Checkpoint. Working on mount and unmount.

--- src/js/include.js
+++ src/js/include.js
@@ -1,33 +1,33 @@
 class IncludeNode {
     constructor(name) {
-        this.name = name;
-        this.children = [];
-        this.parent = null;
+        this.name = name
+        this.children = []
+        this.parent = null
     }
     addChild(child) {
-        let node = new IncludeNode(child);
-        node.parent = this;
-        this.children.push(node);
-        return node;
+        let node = new IncludeNode(child)
+        node.parent = this
+        this.children.push(node)
+        return node
     }
     hasChild(name) {
-        let node = this.getChildByName(name);
-        return (null !== node); 
+        let node = this.getChildByName(name)
+        return (null !== node) 
     }
     getChildByName(name) {
         for (let node of this.children) {
-            if (node.name === name) { return node; }
+            if (node.name === name) { return node }
         }
-        return null;
+        return null
     }
     hasAncestor(ancestorName) {
         let node = this.getAncestor(ancestorName)
         return (null !== node)
     }
     getAncestor(ancestorName) {
-        if (!this.parent) {return null;}
-        if (this.parent.name === ancestorName) {return this.parent;}
-        return this.parent.getAncestor(ancestorName);
+        if (!this.parent) {return null}
+        if (this.parent.name === ancestorName) {return this.parent}
+        return this.parent.getAncestor(ancestorName)
     }
     hasDescendant(descendantName) {
         let node = this.getDescendant(descendantName)
@@ -35,96 +35,52 @@ class IncludeNode {
     }
     getDescendant(descendantName) {
         for (let node of this.children) {
-            if (node.name === descendantName) { return node; }
-            let descendant = node.getDescendant(descendantName);
-            if (descendant) { return descendant; }
+            if (node.name === descendantName) { return node }
+            let descendant = node.getDescendant(descendantName)
+            if (descendant) { return descendant }
         }
-        return null;
+        return null
     }
     toString(prefix) {
-        let result = "" + prefix + this.name + " (parent: " + this.parent?.name + ")\n";
-        prefix += "\t";
+        let result = `${prefix}${this.name} (parent: ${this.parent?.name})\n`
+        prefix += `\t`
         for (let node of this.children) {
-            result += node.toString(prefix);
+            result += node.toString(prefix)
         }
-        return result;
+        return result
     }
 }
 class IncludeTree {
     constructor() {
-        this.nodes = [];
+        this.nodes = []
     }
     addNode(node) {
         if (this.hasNode(node.name)) { return }
-        this.nodes.push(node);
+        this.nodes.push(node)
     }
     hasNode(name) {
-        let node = this.getNodeByName(name);
-        return (null !== node); 
+        let node = this.getNodeByName(name)
+        return (null !== node) 
     }
     getNodeByName(name) {
         for (let node of this.nodes) {
-            if (node.name === name) { return node; }
-            let descendant = node.getDescendant(name);
-            if (descendant) { return descendant; }
+            if (node.name === name) { return node }
+            let descendant = node.getDescendant(name)
+            if (descendant) { return descendant }
         }
-        return null;
+        return null
     }
     toString(prefix) {
-        let result = ""
+        let result = ``
         for (let node of this.nodes) {
             result += node.toString(prefix)
         }
-        return result;
+        return result
     }
 }
-  
-const loadIncludes = () => {
-    const _loadFile = (filename, callback) => {
-        fetch(filename)
-            .then(response => response.text())
-            .then(text => callback(text));
-    };
-    const _loadIncludes = (previousIncludeTree) => {
-        let includes = document.getElementsByTagName('include');
-        if (0 === includes.length) {return}
-        let includeTree = previousIncludeTree;
-        let include = includes[0];
-        let src = include.attributes?.src?.value;
-        let thisfile = include.attributes?.thisfile?.value;
-        let node = includeTree.hasNode(thisfile)? includeTree.getNodeByName(thisfile) : new IncludeNode(thisfile);
-        includeTree.addNode(node);
-        if (node.hasAncestor(src)) {
-            console.error("Include tag causes infinite recursion. Include processing halted. File containg the bad include tag: " + thisfile + ". Include file causing recursion: " + src);
-            return;
-        }
-        _loadFile(include.attributes.src.value, function(text){
-            include.insertAdjacentHTML('afterend', text);
-            include.remove();
-            _loadIncludes(includeTree);
-        });
-    }
-    const _validateVIncudeAttributes = (attributes) => {
-        let src = attributes?.src?.value;
-        let thisfile = attributes?.thisfile?.value;
-        let component = attributes?.component?.value;
-        let id = attributes?.id?.value;
 
-        if (!src) {
-            console.error("V-include missing required attribute 'src'. V-include processing halted. File containing the bad include tag: " + thisfile + ".");
-            return false;
-        }
-        if (!thisfile) {
-            console.error("V-include missing required attribute 'thisfile'. V-include processing halted. Include file causing recursion: " + src);
-            return false;
-        }
-        if (!component) {
-            console.error("V-include missing required attribute 'component'. V-include processing halted. File containing the bad include tag: " + thisfile + ". Include file causing recursion: " + src + ".");
-            return false;
-        }
-        return [src, thisfile, component, id]
-    };
-    const _replaceNodeValue = (node, data, member) => {
+class VanillaComponent {
+    static replaceNodeValue = (node, data, member) => {
         if (node.nodeValue) {
             if (!node.originalNodeValue) { node.originalNodeValue = node.nodeValue }
 
@@ -134,10 +90,10 @@ const loadIncludes = () => {
             }
         }
         for (let child of node.childNodes) {
-            _replaceNodeValue(child, data, member)
+            VanillaComponent.replaceNodeValue(child, data, member)
         }
     }
-    const _replaceAttributeValue = (node, data, member) => {
+    static replaceAttributeValue = (node, data, member) => {
         if (node.attributes) {
             for (const attr of node.attributes) {
                 if (!attr.originalAttributeValue) { attr.originalAttributeValue = attr.value }
@@ -148,101 +104,204 @@ const loadIncludes = () => {
             }
         }
         for (let child of node.childNodes) {
-            _replaceAttributeValue(child, data, member)
+            VanillaComponent.replaceAttributeValue(child, data, member)
         }
     }
-    const _wrapProps = (componentObject, fragment) => {
-        let members = Object.getOwnPropertyNames(componentObject.props);
+    static wrapProps = (componentObject, fragment) => {
+        let members = Object.getOwnPropertyNames(componentObject.props)
 
         componentObject.props.$propsStore = {...componentObject.props}
         for (let member of members) {
             Object.defineProperty(componentObject.props, member, {
                 get: function() {
-                    return componentObject.props.$propsStore[member];
+                    return componentObject.props.$propsStore[member]
                 },
                 set: function(newValue) {
                 }
             })
-            _replaceNodeValue(fragment, componentObject.props, member)
-            _replaceAttributeValue(fragment, componentObject.props, member)
+            VanillaComponent.replaceNodeValue(fragment, componentObject.props, member)
+            VanillaComponent.replaceAttributeValue(fragment, componentObject.props, member)
         }
     }
-    const _wrapVars = (componentObject, fragment) => {
-        let members = Object.getOwnPropertyNames(componentObject.vars);
+    static wrapVars = (componentObject, fragment) => {
+        let members = Object.getOwnPropertyNames(componentObject.vars)
 
         componentObject.vars.$varsStore = {...componentObject.vars}
         for (let member of members) {
             Object.defineProperty(componentObject.vars, member, {
                 get: function() {
-                    return componentObject.vars.$varsStore[member];
+                    return componentObject.vars.$varsStore[member]
                 },
                 set: function(newValue) {
-                    componentObject.vars.$varsStore[member] = newValue;
-                    _replaceNodeValue(fragment, componentObject.vars, member)
-                    _replaceAttributeValue(fragment, componentObject.vars, member)
+                    componentObject.vars.$varsStore[member] = newValue
+                    VanillaComponent.replaceNodeValue(fragment, componentObject.vars, member)
+                    VanillaComponent.replaceAttributeValue(fragment, componentObject.vars, member)
                 }
             })
-            _replaceNodeValue(fragment, componentObject.vars, member)
-            _replaceAttributeValue(fragment, componentObject.vars, member)
+            VanillaComponent.replaceNodeValue(fragment, componentObject.vars, member)
+            VanillaComponent.replaceAttributeValue(fragment, componentObject.vars, member)
         }
     }
-    const _registerComponent = (id, componentObject) => {
+    static registerComponent = (id, componentObject) => {
         if (!id) { return false }
         if (!document.$vanilla) { document.$vanilla = {} }
-        if (!document.$vanilla.registry) { document.$vanilla.registry = new Map(); }
-        document.$vanilla.registry.set(id, componentObject)
+        if (!document.$vanilla.registry) { document.$vanilla.registry = new Map() }
+        const node = document.getElementById(id)
+        if (!node) {
+            console.error(`Failed to register component. Could not find ${id}.`)
+            return
+        }
+        document.$vanilla.registry.set(id, {componentObject, node})
         return true
     }
-    const _loadVIncludes = (previousIncludeTree) => {
-        let includes = document.getElementsByTagName('v-include');
+    static mountComponent = (id) => {
+        const componentInfo = document.$vanilla.registry.get(id)
+        if (!componentInfo) {
+            console.error(`Failed to mount component. Could not find ${id}.`)
+            return
+        }
+        const marker = document.getElementById(`-VanillaComponent${id}`)
+        if (!marker) {
+            console.error(`Failed to mount component. Could not find DOM location of ${id}.`)
+            return
+        }
+        componentInfo.componentObject.beforeMount()
+        if (marker.nextSibling){ 
+            marker.parentNode.insertBefore(componentInfo.node, marker.nextSibling);
+            console.log(`insertBefore`)
+        } else {
+            marker.parentNode.appendChild(componentInfo.node);
+            console.log(`appendChild`)
+
+        }
+        componentInfo.componentObject.afterMount()
+    }
+    static unmountComponent = (id) => {
+        const componentInfo = document.$vanilla.registry.get(id)
+        if (!componentInfo) {
+            console.error(`Failed to unmount component. Could not find ${id}.`)
+            return
+        }
+        const marker = document.getElementById(`-VanillaComponent${id}`)
+        if (!marker) {
+            console.error(`Failed to unmount component. Could not find DOM location of ${id}.`)
+            return
+        }
+        componentInfo.componentObject.beforeUnmount()
+        marker.parentNode.removeChild( marker.nextSibling );
+        componentInfo.componentObject.afterUnmount()
+    }
+}
+
+const loadFile = (filename, callback) => {
+    fetch(filename)
+        .then(response => response.text())
+        .then(text => callback(text))
+}
+
+const loadIncludes = () => {
+    const _loadIncludes = (previousIncludeTree) => {
+        let includes = document.getElementsByTagName('include')
         if (0 === includes.length) {return}
-        let includeTree = previousIncludeTree;
-        let include = includes[0];
+        let includeTree = previousIncludeTree
+        let include = includes[0]
+        let src = include.attributes?.src?.value
+        let thisfile = include.attributes?.thisfile?.value
+        let node = includeTree.hasNode(thisfile)? includeTree.getNodeByName(thisfile) : new IncludeNode(thisfile)
+        includeTree.addNode(node)
+        if (node.hasAncestor(src)) {
+            console.error(`Include tag causes infinite recursion. Include processing halted. File containg the bad include tag: ${thisfile}. Include file causing recursion: ${src}.`)
+            return
+        }
+        loadFile(include.attributes.src.value, function(text){
+            include.insertAdjacentHTML('afterend', text)
+            include.remove()
+            _loadIncludes(includeTree)
+        })
+    }
+    _loadIncludes(new IncludeTree())
+}
+
+const loadVIncludes = () => {
+    const _validateVIncudeAttributes = (attributes) => {
+        let src = attributes?.src?.value
+        let thisfile = attributes?.thisfile?.value
+        let component = attributes?.component?.value
+        let id = attributes?.id?.value
+
+        if (!src) {
+            console.error(`V-include missing required attribute 'src'. V-include processing halted. File containing the bad include tag: ${thisfile}.`)
+            return false
+        }
+        if (!thisfile) {
+            console.error(`V-include missing required attribute 'thisfile'. V-include processing halted. Include file causing recursion: ${src}.`)
+            return false
+        }
+        if (!component) {
+            console.error(`V-include missing required attribute 'component'. V-include processing halted. File containing the bad include tag: ${thisfile}. Include file causing recursion: ${src}.`)
+            return false
+        }
+        return [src, thisfile, component, id]
+    }
+    const _loadVIncludes = (previousIncludeTree) => {
+        let includes = document.getElementsByTagName('v-include')
+        if (0 === includes.length) {return}
+        let includeTree = previousIncludeTree
+        let include = includes[0]
         let [src, thisfile, component, id, repeat] = _validateVIncudeAttributes(include.attributes)
         if (!src || !thisfile || !component) {return}
-        let node = includeTree.hasNode(thisfile)? includeTree.getNodeByName(thisfile) : new IncludeNode(thisfile);
-        includeTree.addNode(node);
+        let node = includeTree.hasNode(thisfile)? includeTree.getNodeByName(thisfile) : new IncludeNode(thisfile)
+        includeTree.addNode(node)
         if (node.hasAncestor(src)) {
-            console.error("V-include tag causes infinite recursion. V-include processing halted. File containing the bad include tag: " + thisfile + ". Include file causing recursion: " + src + ".");
-            return;
+            console.error(`V-include tag causes infinite recursion. V-include processing halted. File containing the bad include tag: ${thisfile}. Include file causing recursion: ${src}.`)
+            return
         }
-        _loadFile(include.attributes.src.value, function(text){
-            var frag = document.createDocumentFragment();
+        loadFile(include.attributes.src.value, function(text){
+            var frag = document.createDocumentFragment()
 
-            frag.append(...new DOMParser().parseFromString(text, "text/html").body.childNodes);
-            const scripts = frag.querySelectorAll("script")
+            frag.append(...new DOMParser().parseFromString(text, `text/html`).body.childNodes)
+            const scripts = frag.querySelectorAll(`script`)
+            const tests = frag.querySelectorAll(`test`)
             try {
-                var script_tag = document.createElement('script');
-                script_tag.type = 'text/javascript';
-                script_tag.appendChild(document.createTextNode(scripts[0].innerText));
-                include.after(script_tag);
+                var script_tag = document.createElement('script')
+                script_tag.type = 'text/javascript'
+                script_tag.appendChild(document.createTextNode(scripts[0].innerText))
+                include.after(script_tag)
             } catch (e) {
-                console.error(`V-include tag failed to create script node.`);
-                return;
+                console.error(`V-include tag failed to create script node.`)
+                return
             }
             for (let scriptNode of scripts){
                 scriptNode.remove()
             }
-            
-            let componentObject = eval(` new ${component}()`);
-            if (!componentObject) {
-                console.error("Failed to create component. V-include processing halted. Component name: " + component + ". File containing the bad include tag: " + thisfile + ". Include file causing recursion: " + src + ".");
-                return;
+            for (let testNode of tests){
+                testNode.remove()
             }
-            _registerComponent(id, componentObject)
-            _wrapVars(componentObject, frag)
-            _wrapProps(componentObject, frag)
-            componentObject.initialize();
-            componentObject.beforeMount();
-            include.after(frag);
-            include.remove();
-            componentObject.afterMount();
             
-            _loadVIncludes(includeTree);
-        });
+            let componentObject = eval(` new ${component}()`)
+            if (!componentObject) {
+                console.error(`Failed to create component. V-include processing halted. Component name: ${component}. File containing the bad include tag: ${thisfile}. Include file causing recursion: ${src}.`)
+                return
+            }
+            componentObject.initialize()
+            VanillaComponent.wrapVars(componentObject, frag)
+            VanillaComponent.wrapProps(componentObject, frag)
+            componentObject.beforeMount()
+            include.after(frag)
+            let marker = document.createElement('script')
+            marker.id = `-VanillaComponent${id}`
+            marker.type = 'text/javascript'
+            include.after(marker)
+            
+            VanillaComponent.registerComponent(id, componentObject)
+            componentObject.afterMount()
+            include.remove()
+
+            _loadVIncludes(includeTree)
+        })
     }
-    _loadIncludes(new IncludeTree());
-    _loadVIncludes(new IncludeTree());
+    _loadVIncludes(new IncludeTree())
 }
 
-document.addEventListener("DOMContentLoaded", loadIncludes);
+document.addEventListener(`DOMContentLoaded`, loadIncludes)
+document.addEventListener(`DOMContentLoaded`, loadVIncludes)
